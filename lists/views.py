e5202178d379bfc83cfde685e958a6914b50ae83
EmanueleHassan <marco.hassan30@gmail.com>
2024-03-22 12:25:19 +0100

    View handling post request via template variable

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, "home.html")
+
+    return render(
+        request,
+        "home.html",
+
+        # The third argument passes variables to templates.  Here you
+        # pass the variable upon a post request.  We use dict.get to
+        # supply a default value, for the case where we are doing a
+        # normal GET request, when the POST dictionary is empty.
+        {"new_item_text": request.POST.get("item_text", "")} 
+    )
 
